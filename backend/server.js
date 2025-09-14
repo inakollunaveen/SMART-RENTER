2a6305a3e102c69d9e75badd057bc183dc70027f
Inakollu Venkata Naveen Kumar <inakolluvenkatanaveenkumar@gmail.com>
2025-09-14 21:31:11 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,6 +29,7 @@ app.use(
       "http://localhost:8080",
       "http://localhost:3000",
       "http://localhost:5173",
+      "https://smart-renter-05od.onrender.com",
       process.env.CLIENT_URL, // allow .env config for deployed frontend
     ],
     credentials: true,
