b4c938e2e37516451e84fdc00a5d933ca8cf982d
Inakollu Venkata Naveen Kumar <inakolluvenkatanaveenkumar@gmail.com>
2025-09-15 22:41:26 +0530

    Update HeroSection.tsx

--- frontend/src/components/hero/HeroSection.tsx
+++ frontend/src/components/hero/HeroSection.tsx
@@ -87,7 +87,7 @@ const HeroSection = () => {
                 <SelectItem value="2bhk">2 BHK</SelectItem>
                 <SelectItem value="3bhk">3 BHK</SelectItem>
                 <SelectItem value="4bhk">4 BHK</SelectItem>
-                <SelectItem value="VILLA">VILLA</SelectItem>
+                <SelectItem value="villa">VILLA</SelectItem>
               </SelectContent>
             </Select>
 
